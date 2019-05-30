3b948c6956fb7ffa52ebf7218240b7644ff941b7
IYefremov <iyefremov@cyberiansoft.com>
2019-05-30 18:31:09 +0300

    Initial commit

---
+++ src/main/java/com/less3task5Package/Minimum.java
@@ -0,0 +1,62 @@
+
+//Write a function which calculates and returns the minimum of 4 arguments.
+//public static void main(String[] args) {
+//        // write your code here
+//        }
+//
+//
+//public static int min(int a, int b, int c, int d) {
+//
+//        }
+//public static int min(int a, int b) {
+//
+//        }
+//Function min(int a, int b, int c, int d) should use the function
+
+
+
+package com.less3task5Package;
+
+public class Minimum {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = -2;
+        int z = 3;
+        int q = 0;
+
+        int minRes = min(x,y,z,q);
+        System.out.println("The minimum among " + x + " " + y + " " + z + " " + q + " is: " + minRes);
+    }
+
+
+    public static int min(int a, int b, int c, int d) {
+        int minim = a;
+        int tmp;
+
+        tmp = min(a,b);
+        if (tmp < minim) {
+            minim = tmp;
+        }
+        tmp = min(minim, c);
+        if (tmp < minim){
+            minim = tmp;
+        }
+        tmp = min (minim, d);
+        if (tmp < minim){
+            minim = tmp;
+        }
+
+        return minim;
+    }
+
+    public static int min(int a, int b) {
+        if (a <= b){
+            return a;
+        } else return b;
+
+    }
+
+}
+
+
